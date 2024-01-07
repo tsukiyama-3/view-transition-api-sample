0e4dfb1f9ec7bc8073de16ef16930842055cd01a
tsukiyama-3 <tsukiyama.mail@gmail.com>
2024-01-07 19:28:42 +0900

    install tailwind

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,8 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true }
+  devtools: { enabled: true },
+  postcss: {
+    plugins: { tailwindcss: {} },
+  },
+  css: ['~/assets/css/tailwind.css'],
 })
